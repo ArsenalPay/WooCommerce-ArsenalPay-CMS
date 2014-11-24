1e7229d7e886cb335bfe4bb2c6267de95ffd981f
Natalya Li <pro.nata90@yandex.ru>
2014-11-25 10:00:08 +1100

    v1.0.0: src files

---
+++ ReadMe.md
@@ -0,0 +1,115 @@
+# ArsenalPay Plugin for Woocommerce of Wordpress CMS
+
+[Arsenal Media LLC](http://www.arsenalmedia.ru/index.php/en)
+
+[Arsenal Pay processing server]( https://arsenalpay.ru/)
+
+
+
+## Version
+1.0.0
+
+*Has been tested on WordPress 4.0/4.0.1 and WooCommerce 2.2.4/2.2.8*
+
+
+Basic feature list:
+
+ * Allows seamlessly integrate unified payment frame into your site.
+ * New payment method ArsenalPay will appear to pay for your products and services.
+ * Allows to pay using mobile commerce and bank aquiring. More methods are about to become available. Please check for updates.
+ * Supports two languages (Russian, English).
+ 
+## How to install
+1. Download zip archive from `htpp://github/ ` .
+2. Login to the WordPress admin section. 
+3. Go to **Plugins>Add New>Upload Plugin** and choose the right location of downloaded plugin archive.
+4. Click **Install Now** and then **Activate Plugin**.
+
+
+## Settings
+1. Go to **WooCommerce>Settings>Checkout**.
+2. There choose **ArsenalPay** method.
+3. Make following settings:
+ - Check the box next to **Enable ArsenalPay**.
+ - You can edit **Title** and **Description** of ArsenalPay payment method as you would like to display it at your site.
+ - Fill out **Unique token**, **Sign key** fields with your received token and key.
+ - Check **Frame URL** to be as `https://arsenalpay.ru/payframe/pay.php`
+ - Choose payment type in **src parameter** as `card` to activate payments with bank cards or `mk` to activate payments from mobile phone accounts.
+ - **css parameter**. You can specify CSS file to apply it to the view of payment frame by inserting css-file url.
+ - You can specify IP address only from which it will be allowed to receive callback requests about payments from ArsenalPay onto your site in **Allowed IP address** field.
+ - Your online shop will be receiving callback requests about processed payments for automatically order status change. The callbacks will being received onto the address assigned in **Callback URL** string upside of the payment plugin settings. Callback is set to address: `http(s)://yourSiteAddress/?wc-api=wc_gw_arsenalpay&arsenalpay=callback`
+ - If it is needed to add one more step to check a payer order number before payment processing you should fill out the field of **Check URL** in the module settings with url-address to which ArsenalPay will be sending requests with check parameters. By default the address is the same with **Callback URL**. 
+ - Set **Frame mode** as `1` to display payment frame inside your site, otherwise a payer will be redirected directly to the payment frame url.
+ - You can adjust **width**, **height**, **frameborder** and **scrolling** of ArsenalPay payment frame by setting iframe parameters. For instance, you can insert string in format: `width="100%" height"500" frameborder="0" scrolling="no"`. Go to html standard reference for more detailes about iframe parameters.
+ - You can enable/disable logging by checking/unchecking the box.
+5. Finally, save your settings by clicking on **Save Changes**
+
+
+## How to uninstall
+1. Go to **Plugins** in WordPress admin section and find **ArsenalPay** in plugin list.
+2. Click on **Deactivate**. 
+3. Further you can delete files from your server by clicking on **Delete** and submitting the fact of deletion.
+
+## Usage
+After successful installation and proper settings new choice of payment method with ArsenalPay will appear on your site. To make payment for an order a payer will need to:
+
+1. Choose goods from the shop catalog.
+2. Go into the order page.
+3. Choose the ArsenalPay payment method.
+4. Check the order detailes and confirm the order.
+5. After filling out the information depending on the payment type he will receive SMS about payment confirmation or will be redirected to the page with the result of his payment.
+
+------------------
+### О ПЛАГИНЕ
+* Плагин платежной системы ArsenalPay под WooCommerce для WordPress позволяет легко встроить платежную страницу на Ваш сайт.
+* После установки плагина у Вас появится новый вариант оплаты товаров и услуг через платежную систему ArsenalPay.
+* Платежная система ArsenalPay позволяет совершать оплату с различных источников списания средств: мобильных номеров (МТС/Мегафон/Билайн/TELE2), пластиковых карт (VISA/MasterCard/Maestro). Перечень доступных источников средств постоянно пополняется. Следите за обновлениями.
+* Плагин поддерживает русский и английский языки.
+
+За более подробной информацией о платежной системе ArsenalPay обращайтесь по адресу [arsenalpay.ru](http://arsenalpay.ru)
+
+### УСТАНОВКА
+1. Загрузите zip архив плагина из репозитория `http://github/` ;
+2. Зайдите в администрирование WordPress;
+3. Пройдите в **Плагины>Добавить новый>Загрузить плагин** и выберите файл с загруженным архивом;
+4. Нажмите на **Установить** и затем **Активировать плагин**.
+
+### НАСТРОЙКА
+1. В администрировании WordPress пройдите в **WooCommerce>Настройки>Оплата**.
+2. Выберите закладку **ArsenalPay**.
+3. Проведите следуюшие настройки:
+  - Проставьте галочку возле **Включить ArsenalPay**.
+  - Вы можете изменить **Заголовок** и **Описание** платежного метода ArsenalPay так, как Вам хотелось бы отобразить его на Вашем сайте.
+  - Заполните поля **Уникальный токен** и **Ключ**, присвоенными Вам токеном и ключом для подписи.
+ - Проверьте: **URL-адрес фрейма** должен быть установлен как `https://arsenalpay.ru/payframe/pay.php`
+ - Установите тип оплаты в поле **Параметр src** как `card` для активации платежей с пластиковых карт или  как `mk` — платежей с аккаунтов мобильных телефонов.
+ - Вы можете задать **Параметр css** для применения к отображению платежного фрейма, указав url css-файла.
+ - Вы можете задать ip-адрес, только с которого будут разрешены обратные запросы о совершаемых платежах, в поле **Разрешенный IP-адрес**.
+ - Ваш интернет-магазин будет получать уведомления о совершенных платежах. На адрес, указанный в поле **Callback URL** вверху настроек плагина, от ArsenalPay будет поступать запрос с результатом платежа для фиксирования статусов заказа в системе предприятия. Обратный запрос настроен на адрес: `http(s)://адресВашегоСайта/?wc-api=wc_gw_arsenalpay&arsenalpay=callback`
+ - При необходимости добавления дополнительного шага с проверкой номера заказа перед проведением платежа, Вы должны заполнить поле **URL для проверки**, на который от ArsenalPay будет поступать запрос на проверку. По умолчанию значение совпадает с **Callback URL**.
+ - Вы можете устанавливать **Режим отображения фрейма**. Значение `1` соответствует отображению фрейма внутри Вашего сайта , иначе пользователь будет перенаправляться напрямую на адрес платежного фрейма.
+ - **Дополнительные параметры фрейма**. Вы можете подгонять ширину, высоту, границу и прокрутку платежного фрейма, задавая соответствующие значения параметров iframe в формате `width="100%" height"500" frameborder="0" scrolling="no"`. За более пдоробной информацией о параметрах iframe обращайтесь к стандарту html.
+ - Вы можете включать/выключать протоколирование для управления сохранением логов для отладки.
+7. Закончив, сохраните настройки нажатием на **Сохранить изменения**.
+
+### УДАЛЕНИЕ
+1. Пройдите в раздел **Плагины** в администрировании WordPress;
+2. Найдите **ArsenalPay** в списке плагинов;
+2. Нажмите на **Деактивировать**;
+3. Затем Вы можете удалить файлы с Вашего сервера, нажав на **Удалить** и подтвердив удаление.
+
+### ИСПОЛЬЗОВАНИЕ
+После успешной установки и настройки плагина на сайте появится возможность выбора платежной системы ArsenalPay.
+Для оплаты заказа с помощью платежной системы ArsenalPay нужно:
+
+1. Выбрать из каталога товар, который нужно купить.
+2. Перейти на страницу оформления заказа (покупки).
+3. В разделе "Платежные системы" выбрать платежную систему ArsenalPay.
+4. Перейти на страницу подтверждения введенных данных и ввода источника списания средств (мобильный номер, пластиковая карта и т.д.).
+5. После ввода данных об источнике платежа, в зависимости от его типа, либо придет СМС о подтверждении платежа, либо покупатель будет перенаправлен на страницу с результатом платежа.
+ 
+
+
+
+
+ 
--- wc-arsenalpay/wc-arsenalpay.php
+++ wc-arsenalpay/wc-arsenalpay.php
@@ -4,32 +4,34 @@ Plugin Name: ArsenalPay
 Plugin URI: https://arsenalpay.ru/site/cmsplugins
 Description: Extends WooCommerce with ArsenalPay gateway.
 Version: 1.0.0
-Author: ArsenalPay
+Author: Arsenal Media Dev.
 Author URI: https://arsenalpay.ru
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: wc-arsenalpay
+Domain Path: /languages/
 */
 
 //Tips for plugin developer:
 //Don't use any cyrillic symbols in comments to display this code properly in wordpress plugin-editor after clicking on /change the plugin/.
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-add_action('plugins_loaded', 'wc_arsenalpay_gateway_init', 0);
+add_action('plugins_loaded', 'wc_arsenalpay_init', 0);
  
-function wc_arsenalpay_gateway_init() 
+function wc_arsenalpay_init() 
 {
     if ( !class_exists( 'WC_Payment_Gateway' ) ) return;
-	if(class_exists('WC_Gateway_Arsenalpay'))
+	if(class_exists('WC_GW_Arsenalpay'))
 		return;
     /**
-     * Localisation
+     * Localisation, function responsible for translation
      */
-   // load_plugin_textdomain('wc-gateway-arsenalpay', false, dirname( plugin_basename( __FILE__ ) ) . '/languages');
+    load_plugin_textdomain('wc-arsenalpay', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/'); 
 
     /**
      * Gateway class
      */
-    class WC_Gateway_Arsenalpay extends WC_Payment_Gateway 
+    class WC_GW_Arsenalpay extends WC_Payment_Gateway 
     {
         public function __construct() 
         {
@@ -53,7 +55,6 @@ function wc_arsenalpay_gateway_init()
             $this->arsenalpay_key	   = $this->get_option( 'arsenalpay_key' );
             $this->arsenalpay_css          = $this->get_option( 'arsenalpay_css' );
             $this->arsenalpay_ip           = $this->get_option( 'arsenalpay_ip' );
-            $this->arsenalpay_callback_url = $this->get_option( 'arsenalpay_callback_url' );
             $this->arsenalpay_check_url    = $this->get_option( 'arsenalpay_check_url' );
             $this->arsenalpay_src	   = $this->get_option( 'arsenalpay_src' );
             $this->arsenalpay_frame_url    = $this->get_option( 'arsenalpay_frame_url' ) ;
@@ -70,11 +71,11 @@ function wc_arsenalpay_gateway_init()
             add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
 
             // ArsenalPay callback hook:
-            add_action( 'woocommerce_api_wc_gateway_arsenalpay', array( $this, 'callback_listener' ) );
+            add_action( 'woocommerce_api_wc_gw_arsenalpay', array( $this, 'callback_listener' ) );
             
             if ( ! $this->is_valid_for_use() ) 
             {
-		$this->enabled = false;
+				$this->enabled = false;
             }
         }
         
@@ -100,7 +101,7 @@ function wc_arsenalpay_gateway_init()
                 'enabled' => array(
                     'title'   => __( 'Enable/Disable', 'woocommerce' ),
                     'type'    => 'checkbox',
-                    'label'   => __( 'Enable ArsenalPay', 'woocommerce' ),
+                    'label'   => __( 'Enable ArsenalPay', 'wc-arsenalpay' ),
                     'default' => 'yes'
                     ),
                 'title' => array(
@@ -113,82 +114,75 @@ function wc_arsenalpay_gateway_init()
                 'description' => array(
                     'title'       => __( 'Description', 'woocommerce' ),
                     'type'        => 'text',
-                    'default'     => __( 'Pay via ArsenalPay with your bank card or mobile phone number.', 'woocommerce' ),
+                    'default'     => __( 'Pay with ArsenalPay.', 'wc-arsenalpay' ),
                     'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
                     'desc_tip'    => true,
                     ),
                 'arsenalpay_token' => array(
-                    'title'       => __( 'Unique token', 'woocommerce' ),
+                    'title'       => __( 'Unique token', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( 'Assigned to merchant for the access to ArsenalPay payment frame, required', 'woocommerce' ),
+                    'description' => __( 'Assigned to merchant for the access to ArsenalPay payment frame. Required.', 'wc-arsenalpay' ),
                     'desc_tip'    => true,			
                     ),
                 'arsenalpay_other_code' => array(
-                    'title'       => __( 'Other number or code requered for making payments.', 'woocommerce' ),
+                    'title'       => __( 'Other number or code required for making payments', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( 'Not accessible for editing to the user and not displayed if it is set, optional.', 'woocommerce' ),
+                    'description' => __( 'Not accessible for editing to the user and not displayed if it is set. Optional.', 'wc-arsenalpay' ),
                     'desc_tip'    => true,			
-                    ),
-                'arsenalpay_key' => array(
-                    'title'       => __( 'Sign key', 'woocommerce' ),
+                    ),	
+                'arsenalpay_frame_url' => array(
+                    'title'       => __( 'Frame URL', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( 'Key to check the request sign, required.', 'woocommerce' ),
+                    'description' => __( 'URL-address of ArsenalPay payment frame. Required.', 'wc-arsenalpay' ),
+                    'default'     => 'https://arsenalpay.ru/payframe/pay.php',
                     'desc_tip'    => true,
-                    ),
-                'arsenalpay_css' => array(
-                    'title'       => __( 'css option', 'woocommerce' ),
-                    'type'        => 'text',
-                    'description' => __( 'URL of CSS file, optional.', 'woocommerce' ),
-                    'desc_tip'    => true,
-                    ),
-                'arsenalpay_ip' => array(
-                    'title'       => __( 'IP-address', 'woocommerce' ),
-                    'type'        => 'text',
-                    'description' => __( 'Allowed IP-address to accept ArsenalPay requests from, required.', 'woocommerce' ),
-                    'desc_tip'    => true,
-                    ),
-                'arsenalpay_callback_url' => array(
-                    'title'       => __( 'Callback URL', 'woocommerce' ),
-                    'type'        => 'text',
-                    'description' => __( 'Callback URL of payment, required.', 'woocommerce' ),
-                    'desc_tip'    => true,
-                    'default'     => WC()->api_request_url( 'WC_Gateway_ArsenalPay' ) . '&arsenalpay=callback',
-                    ),
-                'arsenalpay_check_url' => array(
-                    'title'       => __( 'Check URL.', 'woocommerce' ),
-                    'type'        => 'text',
-                    'description' => __( 'URL to check the existance of transaction, optional', 'woocommerce' ),
-                    'desc_tip'    => true,
-                    ),
+                    ),	
                 'arsenalpay_src' => array(
-                    'title'       => __( 'Src parameter', 'woocommerce' ),
+                    'title'       => __( 'src parameter', 'wc-arsenalpay' ),
                     'type'        => 'select',
-                    'description' => __( 'Payment type. Possible options: mk - payment from mobile phone (mobile commerce), card - payment by bank card (internet-acquiring), optional.', 'woocommerce' ),
+                    'description' => __( 'Payment type. Possible options: mk - payment from mobile phone (mobile commerce), card - payment by bank card (internet-acquiring). Optional.', 'wc-arsenalpay' ),
                     'desc_tip'    => true,
                     'default'     => 'mk',
                     'options'     => array(
-                                    'mk'     => __( 'mk', 'woocommerce' ),
-                                    'card'   => __( 'card', 'woocommerce' )
+                                    'mk'     => __( 'mk', 'wc-arsenalpay' ),
+                                    'card'   => __( 'card', 'wc-arsenalpay' )
                                     )
                     ),
-                'arsenalpay_frame_url' => array(
-                    'title'       => __( 'Frame URL', 'woocommerce' ),
+                'arsenalpay_css' => array(
+                    'title'       => __( 'css option', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( 'URL-address of ArsenalPay payment frame, required.', 'woocommerce' ),
-                    'default'     => 'https://arsenalpay.ru/payframe/pay.php',
+                    'description' => __( 'URL of CSS file if exists. Optional.', 'wc-arsenalpay' ),
+                    'desc_tip'    => true,
+                    ),
+                'arsenalpay_key' => array(
+                    'title'       => __( 'Sign key', 'wc-arsenalpay' ),
+                    'type'        => 'text',
+                    'description' => __( 'With this key you check a validity of sign that comes with callback payment data. Required.', 'wc-arsenalpay' ),
+                    'desc_tip'    => true,
+                    ),
+                'arsenalpay_ip' => array(
+                    'title'       => __( 'Allowed IP-address', 'wc-arsenalpay' ),
+                    'type'        => 'text',
+                    'description' => __( 'It can be allowed to receive ArsenalPay payment confirmation callback requests only from the ip address pointed out here. Optional.', 'wc-arsenalpay' ),
+                    'desc_tip'    => true,
+                    ),
+                'arsenalpay_check_url' => array(
+                    'title'       => __( 'Check URL.', 'wc-arsenalpay' ),
+                    'type'        => 'text',
+                    'description' => __( 'URL to check the existance of transaction. Optional', 'wc-arsenalpay' ),
                     'desc_tip'    => true,
                     ),
                 'arsenalpay_frame_mode' => array(
-                    'title'       => __( 'Frame display mode', 'woocommerce' ),
+                    'title'       => __( 'Frame display mode', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( '"1"-to display inside a frame, otherwise fullscreen.', 'woocommerce' ),
+                    'description' => __( '1 - to display inside a frame, otherwise on fullscreen payment frame page. Optional.', 'wc-arsenalpay' ),
                     'desc_tip'    => true,
                     'default'     => '1',
                     ),
                 'arsenalpay_frame_params' => array(
-                    'title'       => __( 'Frame parameters', 'woocommerce' ),
+                    'title'       => __( 'Frame parameters', 'wc-arsenalpay' ),
                     'type'        => 'text',
-                    'description' => __( 'Parameters of iFrame.', 'woocommerce' ),
+                    'description' => __( 'Parameters of iFrame. Optional.', 'wc-arsenalpay' ),
                     'desc_tip'    => true,
                     'default'	  => 'width = "700" height = "500" border = "0" scrolling = "auto"',
                     ),
@@ -197,7 +191,7 @@ function wc_arsenalpay_gateway_init()
                     'type'        => 'checkbox',
                     'label'       => __( 'Enable logging', 'woocommerce' ),
                     'default'     => 'no',
-                    'description' => sprintf( __( 'Log ArsenalPay events, such as callback requests', 'woocommerce' ), wc_get_log_file_path( 'arsenalpay' ) )
+                    'description' => sprintf( __( 'Log ArsenalPay events, such as callback requests', 'wc-arsenalpay' ), wc_get_log_file_path( 'arsenalpay' ) )
                     ),
                 );		
         }
@@ -208,8 +202,9 @@ function wc_arsenalpay_gateway_init()
                 <br>
                 <h3>Redirect URLs</h3>
                     <ul>
-                    <li>Success redirect: <code>".get_bloginfo("url")."/?wc-api=wc_gateway_arsenalpay&arsenalpay=success</code></li>
-                    <li>Failure redirect: <code>".get_bloginfo("url")."/?wc-api=wc_gateway_arsenalpay&arsenalpay=fail</code></li>
+                    <li>".__('Success redirect').": <code>".get_bloginfo("url")."/?wc-api=wc_gw_arsenalpay&arsenalpay=success</code></li>
+                    <li>".__('Failure redirect').": <code>".get_bloginfo("url")."/?wc-api=wc_gw_arsenalpay&arsenalpay=fail</code></li>
+					<li>".__('Callback URL').": <code>".get_bloginfo("url")."/?wc-api=wc_gw_arsenalpay&arsenalpay=callback</code></li>
                     </ul>
                 ";
         
@@ -233,10 +228,20 @@ function wc_arsenalpay_gateway_init()
         {
             $order = wc_get_order( $order_id );
             $total = number_format($order->order_total, 2, '.', '');
+			$url_params = array(
+				'src' => $this->arsenalpay_src,
+				't' => $this->arsenalpay_token,
+				'n' => $order_id,
+				'a' => $total,
+				'msisdn'=> '',
+				'css' => $this->arsenalpay_css,
+				'frame' => $this->arsenalpay_frame_mode,
+                );
+                $f_url = $this->arsenalpay_frame_url;
+                $ap_frame_src = $f_url.'?'.http_build_query($url_params, '', '&');
+				$f_params = $this->arsenalpay_frame_params;
             // Mark as processing (payment won't be taken until delivery)
-            $html = '<iframe name="arspay" src='.$this->arsenalpay_frame_url.'?src='.$this->arsenalpay_src.'&t='.$this->arsenalpay_token.'&n='.$order_id.'&a='.$total.'&css='.$this->arsenalpay_css
-                    .'&frame='.$this->arsenalpay_frame_mode.' '.$this->arsenalpay_frame_params.'></iframe>';
-            //$order->update_status( 'processing', __( 'Payment to be made upon confirmation.', 'woocommerce' ) );
+            $html = '<iframe name="arspay" src='.$ap_frame_src.' '.$f_params.'></iframe>';
             echo $html;
         }
 
@@ -276,7 +281,7 @@ function wc_arsenalpay_gateway_init()
 
                if( !$wc_order || empty($wc_order) )
                 {
-                    if( $ars_callback['FUNCTION']=="check" )
+                    if( $ars_callback['FUNCTION']=="check" && $ars_callback['FUNCTION']=="check")
                     {
                         if ( 'yes' == $this->debug ) 
                         {
@@ -290,7 +295,7 @@ function wc_arsenalpay_gateway_init()
                 {
                     if ( 'yes' == $this->debug )
                     {
-                            $this->log->add( 'arsenalpay', 'Aborting, Order #' . $order->id . ' is already complete.' );
+                            $this->log->add( 'arsenalpay', 'Aborting, Order #' . $wc_order->id . ' is already complete.' );
                     }
                     exit;
                 }
@@ -340,7 +345,7 @@ function wc_arsenalpay_gateway_init()
                         $this->log->add( 'arsenalpay', 'Payment error: Amounts do not match (amount ' . $ars_callback['AMOUNT'] . ')' );
                     }
                     // Put this order on-hold for manual checking
-                    $order->update_status( 'on-hold', sprintf( __( 'Validation error: ArsenalPay amounts do not match (amount %s).', 'woocommerce' ), $ars_callback['AMOUNT'] ) );
+                    $wc_order->update_status( 'on-hold', sprintf( __( 'Validation error: ArsenalPay amounts do not match (amount %s).', 'woocommerce' ), $ars_callback['AMOUNT'] ) );
                     $this->exitf( 'ERR_AMOUNT' );
                 }
 
@@ -351,17 +356,10 @@ function wc_arsenalpay_gateway_init()
                 */
                 if( !( $this->_checkSign( $ars_callback, $KEY) ) ) 
                 {
-				 //============== For testing, delete after testing =============================
-                  $S=md5(md5($ars_callback['ID']).
-                            md5($ars_callback['FUNCTION']).md5($ars_callback['RRN']).
-                          md5($ars_callback['PAYER']).md5($ars_callback['AMOUNT']).md5($ars_callback['ACCOUNT']).
-                           md5($ars_callback['STATUS']).md5($KEY) );
-                    echo $S.'</br>';
-                    //======================================
                     $this->exitf( 'ERR_INVALID_SIGN' );
 
                 }
-                if( $ars_callback['FUNCTION'] == "check" )
+                if( $ars_callback['FUNCTION'] == "check" && $ars_callback['STATUS'] == "check" )
                 {
                     // Check account
                     /*
@@ -370,7 +368,8 @@ function wc_arsenalpay_gateway_init()
                             YES - account exists
                             NO - account not exists
                     */
-
+					$wc_order->update_status( 'on-hold', sprintf( __( 'Order number has been checked.', 'wc-arsenalpay' ) ) );
+					$wc_order->add_order_note( __( 'Waiting for payment confirmation after checking.', 'wc-arsenalpay' ) );
                     $this->exitf( 'YES' );
                 } 
                 elseif( ( $ars_callback['FUNCTION']=="payment" ) && ( $ars_callback['STATUS'] === "payment" ) )
@@ -381,7 +380,7 @@ function wc_arsenalpay_gateway_init()
                             Result:
                             OK - success saving
                             ERR - error saving*/
-                    $wc_order->add_order_note( __( 'Payment completed', 'woocommerce' ) );
+                    $wc_order->add_order_note( __( 'Payment completed', 'wc-arsenalpay' ) );
                     $wc_order->payment_complete();
                     $woocommerce->cart->empty_cart();
                     $this->exitf('OK');  
@@ -434,10 +433,10 @@ function wc_arsenalpay_gateway_init()
     /**
     * Add the ArsenalPay Gateway to WooCommerce
     **/
-    function add_arsenalpay_gateway($methods) {
-            $methods[] = 'WC_Gateway_ArsenalPay';
+    function add_wc_arsenalpay($methods) {
+            $methods[] = 'WC_GW_ArsenalPay';
             return $methods;
     }
 
-    add_filter('woocommerce_payment_gateways', 'add_arsenalpay_gateway' );
+    add_filter('woocommerce_payment_gateways', 'add_wc_arsenalpay' );
 } 
